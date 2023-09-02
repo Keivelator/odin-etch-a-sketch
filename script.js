330e6842367c3ceecbd7e5dd617341563fd010a2
Keivelator <maulidankiki@gmail.com>
2023-09-02 20:10:32 +0700

    made the grid creation button

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const container = document.querySelector('.container');
+let gridSize = 16;
 
-for (i = 1; i <= 16; i++) {
-    const gridRow = document.createElement('div');
-    gridRow.className = 'grid-row';
-    container.appendChild(gridRow);
-    for (j = 1; j <= 16; j++) {
-        const grid = document.createElement('div');
-        grid.className = 'grid';
-        grid.textContent = `${j}`;
-        gridRow.appendChild(grid);
+function createGrid () {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+
+    for (i = 1; i <= gridSize; i++) {
+        const gridRow = document.createElement('div');
+        gridRow.className = 'grid-row';
+        container.appendChild(gridRow);
+        for (j = 1; j <= gridSize; j++) {
+            const grid = document.createElement('div');
+            grid.className = 'grid';
+            grid.textContent = `${j}`;
+            gridRow.appendChild(grid);
+        }
     }
 }
 
+const sidesSelect = document.querySelector('.sides-select');
+sidesSelect.addEventListener('click', () => {
+    gridSize = Number(prompt("Grid size: ", "16"));
+    createGrid();
+})
 
+createGrid ();
 
 
